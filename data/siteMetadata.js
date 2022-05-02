62219160004526eb725fa4bf2d0690811701f326
Nazar Maksymchuk <nzrbeats@gmail.com>
2022-05-01 19:09:28 -0700

    removed email

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -10,7 +10,7 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'nazar@n87m.com',
+  // email: 'nazar@n87m.com',
   github: 'https://github.com/MANTENN',
   twitter: 'https://twitter.com/mantennn',
   // facebook: 'https://facebook.com',
